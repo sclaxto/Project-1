9541a1e6bcb7465abab8532987629f21ca13c150
snclaxto <snclaxto@gmail.com>
2022-03-24 13:23:27 -0400

    updates 3.24

--- js/app.js
+++ js/app.js
@@ -1,26 +1,87 @@
 //constants//
 let colors = ["blue", "orange", "yellow", "black", "brown", "purple", "pink", "gray"];
-
-
+let score = 0;
+let sections = ['#sect1', '#sect2'];
+let userSelection;
+const wrongAnswers = 'null'
 //functions//
 
  function randomColors () {
 let random = colors[Math.floor(Math.random() * colors.length)];
   console.log(random);
-
+  return random
 }
 
-randomColors();
 
-sq1 = randomColors;
-sq2 = randomColors;
+//text color random display selectors//
 
-while (sq1==sq2) {
-    sq2 = randomColors;
+
+//square selectors//
+let s1 = document.querySelector('#sect1')
+s1.style.background = `${randomColors()}`;
+s1.onclick =``
+let s2 = document.querySelector('#sect2')
+s2.style.background = `${randomColors()}`;
+
+//onclick for squares//
+//setting color opposites and equals//
+function setColors(){
+    let colorSet =  document.querySelector('.text')
+    let randomColorWord = randomColors()
+    let randomColorText = randomColors()
+    colorSet.innerHTML = randomColorWord
+    while (randomColorWord === randomColorText) {
+        randomColorText = randomColors()
+    }
+    colorSet.style.color= randomColorText 
+    let randomSection = sections[Math.floor(Math.random() * sections.length)]
+    document.querySelector(randomSection).style.background = randomColorText
+    if(randomSection === '#sect1'){
+        document.querySelector('#sect2').style.background = randomColorWord
+    }else{
+        document.querySelector('#sect1').style.background = randomColorWord
+    }
+
+//gameplay// 
+function init() {
+    
+    if (playerSelects === randomColorWord)
+    return wrongAnswers
 }
 
-//event handler//
-// document.querySelector('#text').innerText = text;
-// document.querySelector('#text').style.color = text;
 
-startGame();
+
+//setting onclick for squares//
+function playerSelects () {
+let squares = document.querySelectorAll('.squares') 
+squares.forEach(square => {
+    square.addEventListener('click', function onclick(e) {
+        userSelection = e.target.innerHTML;
+        console.log(e)
+        return squares 
+    })
+})
+
+
+
+function renderScores() {
+let rightSelection = document.querySelector('#results');
+let wrongSelection = wrongAnswers;
+rightSelection.innerHTML = score;
+if( playerSelects === randomColorText ){ 
+    document.querySelector('#results').innerHTML = score+=1;
+//   let i = 0; i<score.value; i++ 
+}else{
+(playerSelects === wrongSelection)
+return 0;
+
+
+}
+}
+renderScores();
+
+}
+playerSelects();
+
+}
+setColors();
